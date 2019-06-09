3cb94df6b0ca2d79e0366b9774f411f382265e0e
mafalu0606 <1974794628@qq.com>
2019-06-09 16:39:49 +0800

    提交

--- app/src/main/java/com/bawei/myapplication/MainActivity.java
+++ app/src/main/java/com/bawei/myapplication/MainActivity.java
@@ -2,12 +2,15 @@ package com.bawei.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = new Button(this, null);
     }
 }
